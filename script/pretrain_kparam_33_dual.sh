24e97062db4dc5a6881a47bfa9c06066eadcc443
samhu1989 <hsy19891228@yeah.net>
2018-04-26 16:20:56 +0800

    add script

---
+++ script/pretrain_kparam_33_dual.sh
@@ -0,0 +1,2 @@
+python ../run.py gpu=2 net=KPARAM_33_DUAL dump=/data4T1/samhu/tf_dump/ShapeModel/pre pretrain
+python ../run.py gpu=2 net=KPARAM_33_DUAL dump=/data4T1/samhu/tf_dump/ShapeModel/pre train
